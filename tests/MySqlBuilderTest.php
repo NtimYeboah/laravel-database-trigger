f08556be1e5baa438951c4c20e23fb69e9a426f9
Ntim Yeboah <lylegendary@gmail.com>
2018-09-16 20:28:18 +0000

    Add test coverage for mysql builder

---
+++ tests/MySqlBuilderTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace NtimYeboah\LaravelDatabaseTrigger\Test;
+
+use Mockery as m;
+use PHPUnit\Framework\TestCase;
+use NtimYeboah\LaravelDatabaseTrigger\Schema\Grammars\MySqlGrammar;
+use NtimYeboah\LaravelDatabaseTrigger\Schema\MySqlBuilder as Builder;
+
+
+class MySqlBuilderTest extends TestCase
+{
+    public function tearDown()
+    {
+        m::close();
+    }
+
+    public function testTriggerCorrectlyCallsGrammar()
+    {
+        $connection = m::mock('Illuminate\Database\Connection');
+        $grammar = m::mock('stdClass');
+        $connection->shouldReceive('getSchemaGrammar')->andReturn($grammar);
+        $builder = new Builder($connection);
+        $connection->shouldReceive('select')->once()->andReturn(['trigger']);
+
+        $this->assertTrue($builder->hasTrigger('trigger'));
+    }
+}
