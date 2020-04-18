f1f0281aae070920e819c1ca99e9428cf3a99bda
Ntim Yeboah <lylegendary@gmail.com>
2020-04-18 17:32:05 +0000

    Apply fixes from StyleCI

--- tests/MySqlGrammarTest.php
+++ tests/MySqlGrammarTest.php
@@ -9,7 +9,7 @@ use PHPUnit\Framework\TestCase;
 
 class MySqlGrammarTest extends TestCase
 {
-    public function tearDown() :void
+    public function tearDown(): void
     {
         m::close();
     }
